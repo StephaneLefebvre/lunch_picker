ad25ce06f08a14ce6d6990c7e367803a9ea96a76
Stéphane Lefebvre <stephane.lefebvre.ext@mappy.com>
2018-12-18 18:09:37 +0100

    [DEV] Refacto and clean + setup for server

--- lunch_picker.py
+++ lunch_picker.py
@@ -4,53 +4,6 @@ import random
 import statistics
 import operator
 
-
-HTML_PAGE = """
-<!DOCTYPE html>
-<html>
-<body>
-
-<table>
-
-<tr>
-  <td></td>
-  <td>&#9940;</td>
-  <td>&#9785;</td>
-  <td>&#9863;</td>
-  <td>&#9786;</td>
-  <td>&#9924;</td>
-</tr>
-
-{table}
-
-</table>
-
-</body>
-</html>
-"""
-
-TABLE_ELT = """
-<tr>
-  <td>{endroit}</td>
-  <form>
-  <td><input type="radio" name="{endroit}" value=0 checked></td>
-  <td><input type="radio" name="{endroit}" value=1></td>
-  <td><input type="radio" name="{endroit}" value=2></td>
-  <td><input type="radio" name="{endroit}" value=3></td>
-  <td><input type="radio" name="{endroit}" value=4></td>
-  </form>
-</tr>
-"""
-
-
-def create_html_page(restaurant="fake_restaurants"):
-    data = json.load(open("restaurant_list.json"))
-    list_endroit = data[restaurant]
-    supertable = ""
-    for elt in list_endroit:
-        supertable += TABLE_ELT.format(endroit=elt)
-    print(HTML_PAGE.format(table=supertable))
-
 def method_3(restaurant="fake_restaurants_notation"):
     """
     Methode to choose the median
@@ -63,6 +16,9 @@ def method_3(restaurant="fake_restaurants_notation"):
     return max(dict_notation, key=(lambda key: dict_notation[key]))
 
 def method_2(restaurant="fake_restaurants_selection"):
+    """
+    Who as the best grade
+    """
     data = json.load(open("restaurant_list.json"))
     dict_endroit = data[restaurant]
     return max(dict_endroit, key=(lambda key: dict_endroit[key]))
@@ -74,15 +30,5 @@ def method_1(restaurant="fake_restaurants"):
     data = json.load(open("restaurant_list.json"))
     return random.choice(data[restaurant])
 
-def test_methods():
-    """
-    test if code is ok
-    """
-    print(method_1())
-    print(method_2())
-    print(method_3())
-def test_html():
-    create_html_page()
-
-if __name__ == "__main__":
-    test_methods()
+def best_grade(dict_endroit)
+    return max(dict_endroit, key=(lambda key: dict_endroit[key]))
---
+++ tests/unit_methods.py
@@ -0,0 +1,10 @@
+def test_methods():
+    """
+    test if code is ok
+    """
+    print(method_1())
+    print(method_2())
+    print(method_3())
+
+def test_html():
+    create_html_page()
