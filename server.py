629f6adb995f9a390e76fd0daad1a0c577b4fa24
Stéphane Lefebvre <stephane.lefebvre.ext@mappy.com>
2019-01-08 21:43:19 +0100

    [ugly] But functionnal

--- server.py
+++ server.py
@@ -39,9 +39,9 @@ tr:hover {{
 TABLE_ELT = """
 <tr>
   <td><abbr title="{address} à ~{distance} min, pour {prices}€, c'est du {type}">{endroit}</abbr></td>
-  <td><input type="radio" name="{endroit}" value=0 checked></td>
+  <td><input type="radio" name="{endroit}" value=0></td>
   <td><input type="radio" name="{endroit}" value=1></td>
-  <td><input type="radio" name="{endroit}" value=2></td>
+  <td><input type="radio" name="{endroit}" value=2 checked></td>
   <td><input type="radio" name="{endroit}" value=3></td>
   <td><input type="radio" name="{endroit}" value=4></td>
 </tr>
@@ -71,20 +71,36 @@ def vote():
     """ Create the page that process the vote and where the result are displayed """
     dict_endroit = {}
     for restaurant in request.form:
-        if request.form[restaurant] != "0":
+        if request.form[restaurant] != "2":  # optimize network load
             dict_endroit[restaurant] = request.form[restaurant]
     global dict_result
-    dict_result[ip] = dict_endroit
+    dict_result[request.remote_addr] = dict_endroit
 
     agregate_dict = {}
     for dict_endroit in dict_result.values():
         agregate_dict = {
-            k: int(agregate_dict.get(k, 0)) + int(dict_endroit.get(k, 0))
+            k: int(agregate_dict.get(k, 2)) + int(dict_endroit.get(k, 2))
             for k in set(agregate_dict) | set(dict_endroit)
         }
 
     winner = requests.post("http://127.0.0.1:7070/best", data=agregate_dict)
-    return "Ton vote à été pris en compte, le vainqueur actuel est '{}'".format(winner.text)
+    return "Ton vote à été pris en compte, le vainqueur actuel est '{}' with relevant vote being : {}".format(winner.text, json.dumps(agregate_dict))
+
+@APP.route("/vote", methods=["GET"])
+def result():
+    """ Create the page that process the vote and where the result are displayed """
+    global dict_result
+    agregate_dict = {}
+    for dict_endroit in dict_result.values():
+        agregate_dict = {
+            k: int(agregate_dict.get(k, 0)) + int(dict_endroit.get(k, 2))
+            for k in set(agregate_dict) | set(dict_endroit)
+        }
+
+    print(dict_result)
+
+    winner = requests.post("http://127.0.0.1:7070/best", data=agregate_dict)
+    return "Ton vote à été pris en compte, le vainqueur actuel est '{}' with relevant vote being : {}".format(winner.text, json.dumps(agregate_dict))
 
 
 if __name__ == "__main__":
